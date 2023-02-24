9a6cea2fcd84603364585fb4380ff98c79915182
Bizwayo <39114620+Bizwayo@users.noreply.github.com>
2023-02-23 18:36:40 -0600

    refactoring code

--- controllers/main.js
+++ controllers/main.js
@@ -1,24 +1,18 @@
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require('../errors/custom-error')
+const {BadRequestError} = require('../errors')
 
 const login = async (req,res) =>{
     const {username,password} = req.body
-
     if(!username || !password){
-        throw new CustomAPIError('Please provide email and password',400);
+        throw new BadRequestError('Please provide email and password');
     }
-
     const id = new Date().getDate();
-    
     //keep data returned small and DO NOT ADD PERSONAL INFORMATION
     const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'});
-
-
     res.status(200).json({msg: 'user created',token});
 }
 
 const dashboard = async (req,res) => {
-    console.log(req.user);
     const luckyNumber = Math.floor(Math.random()*100);
     res.status(200).json({msg:`Hello, ${req.user.username}`, secret:`Here is your luckyNumber ${luckyNumber}`})
 }
